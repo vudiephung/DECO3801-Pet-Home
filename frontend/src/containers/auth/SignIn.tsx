2af3ef6c283714251aa7b8eeac6c8c2b4412adc2
wildanazz <wildanazzwa@gmail.com>
2021-08-29 02:56:19 +1000

    Add authentication components

--- frontend/src/containers/auth/SignIn.tsx
+++ frontend/src/containers/auth/SignIn.tsx
@@ -1,10 +1,59 @@
-import React from 'react';
-import { View, Button } from 'react-native';
+import React, { memo } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+import Background from '../../components/Background';
+import Logo from '../../components/Logo';
+import Header from '../../components/Header';
+import TextInput from '../../components/TextInput';
+import Button from '../../components/Button';
+import theme from '../../core/theme';
+
+const styles = StyleSheet.create({
+  forgotPassword: {
+    width: '100%',
+    alignItems: 'flex-end',
+    marginBottom: 24,
+  },
+  row: {
+    flexDirection: 'row',
+    marginTop: 4,
+  },
+  label: {
+    color: theme.colors.secondary,
+  },
+  link: {
+    fontWeight: 'bold',
+    color: theme.colors.primary,
+  },
+});
 
 const SignIn = ({ navigation }: any) => (
-  <View>
-    <Button title="Sign Up" onPress={() => navigation.navigate('SignUp')} />
-  </View>
+  <Background>
+    <Logo />
+    <Header>Pet Home</Header>
+    <TextInput
+      label="Email"
+      autoCapitalize="none"
+      autoCompleteType="email"
+      textContentType="emailAddress"
+      keyboardType="email-address"
+    />
+    <TextInput label="Password" secureTextEntry />
+    <View style={styles.forgotPassword}>
+      <TouchableOpacity onPress={() => {}}>
+        <Text style={styles.label}>Forgot your password?</Text>
+      </TouchableOpacity>
+    </View>
+    <Button mode="contained" onPress={() => {}}>
+      Login
+    </Button>
+    <View style={styles.row}>
+      <Text style={styles.label}>Don’t have an account? </Text>
+      <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
+        <Text style={styles.link}>Sign up</Text>
+      </TouchableOpacity>
+    </View>
+  </Background>
 );
 
-export default SignIn;
+export default memo(SignIn);
--- frontend/src/containers/auth/SignUp.tsx
+++ frontend/src/containers/auth/SignUp.tsx
@@ -1,10 +1,50 @@
-import React from 'react';
-import { View, Button } from 'react-native';
+import React, { memo } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+import Background from '../../components/Background';
+import Logo from '../../components/Logo';
+import Header from '../../components/Header';
+import TextInput from '../../components/TextInput';
+import Button from '../../components/Button';
+import theme from '../../core/theme';
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    marginTop: 4,
+  },
+  label: {
+    color: theme.colors.secondary,
+  },
+  link: {
+    fontWeight: 'bold',
+    color: theme.colors.primary,
+  },
+});
 
 const SignUp = ({ navigation }: any) => (
-  <View>
-    <Button title="Sign In" onPress={() => navigation.navigate('SignIn')} />
-  </View>
+  <Background>
+    <Logo />
+    <Header>Pet Home</Header>
+    <TextInput
+      label="Email"
+      autoCapitalize="none"
+      autoCompleteType="email"
+      textContentType="emailAddress"
+      keyboardType="email-address"
+    />
+    <TextInput label="Password" secureTextEntry />
+    <TextInput label="Confirm Password" secureTextEntry />
+    <Button mode="contained" onPress={() => {}}>
+      Sign Up
+    </Button>
+    <View style={styles.row}>
+      <Text style={styles.label}>Already have an account? </Text>
+      <TouchableOpacity onPress={() => navigation.navigate('SignIn')}>
+        <Text style={styles.link}>Sign In</Text>
+      </TouchableOpacity>
+    </View>
+  </Background>
 );
 
-export default SignUp;
+export default memo(SignUp);
