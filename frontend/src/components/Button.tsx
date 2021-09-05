c6acbfe53585fb7d8472e881577a546e3c7b03b8
Quan Nguyen <quan281999@gmail.com>
2021-09-05 10:24:30 +0700

    refactor pet submission form

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -10,7 +10,7 @@ const styles = StyleSheet.create({
   button: {
     width: '100%',
     marginVertical: 10,
-    backgroundColor: '#caff8a',
+    backgroundColor: theme.colors.button,
   },
   text: {
     fontWeight: 'bold',
