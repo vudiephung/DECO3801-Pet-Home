e792b3713942cdec5efc434ede2abe6a336c2e7b
Diep Hung Vu <hungvudiep@gmail.com>
2021-10-14 19:44:56 +1000

    change baseUrl and Home TabNavigator back to adoption screen

--- frontend/src/services/config.ts
+++ frontend/src/services/config.ts
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
-export const baseURL = 'http://10.0.2.2:8081';
-// export const baseURL = 'https://resilience.uqcloud.net'; // Request to and Response from UQ cloud zone
+// export const baseURL = 'http://10.0.2.2:8081';
+export const baseURL = 'https://resilience.uqcloud.net'; // Request to and Response from UQ cloud zone
 // export const baseURL = 'http://10.0.2.2:5000';
 
 const instance = Axios.create({ baseURL });
